b97038db61d06293baeb5433fe90545066de2ec8
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-11-22 01:14:13 +0300

    feat: libre translator integration

---
+++ Translator/Models/Language.cs
@@ -0,0 +1,5 @@
+﻿using LibreTranslate.Net;
+
+namespace Translator.Models;
+
+public record Language(LanguageCode Code, string Name);
---
+++ Translator/Tools/LibreTranslator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LibreTranslate.Net;
+using Translator.Models;
+using LibTranslator = LibreTranslate.Net.LibreTranslate;
+
+namespace Translator.Tools;
+
+public static class LibreTranslator
+{
+    private static readonly LibTranslator Translator = new("http://localhost:5000");
+
+    public static IReadOnlyCollection<Language> SupportedLanguages { get; } = new Language[]
+    {
+        new(LanguageCode.English, "English"),
+        new(LanguageCode.Russian, "Russian"),
+        new(LanguageCode.Arabic, "Arabic"),
+        new(LanguageCode.Japanese, "Japanese"),
+        new(LanguageCode.French, "French"),
+        new(LanguageCode.German, "German"),
+        new(LanguageCode.Spanish, "Spanish")
+    };
+
+    public static async Task<string> TranslateAsync(
+        LanguageCode sourceLanguageCode,
+        LanguageCode targetLanguageCode,
+        string text)
+    {
+        var translateRequest = new Translate
+        {
+            Source = sourceLanguageCode,
+            Target = targetLanguageCode,
+            Text = text
+        };
+
+        return await Translator.TranslateAsync(translateRequest);
+    }
+}
