90a5c7fda29748c04aa5acb2761d54a2205e4dca
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-11-22 01:13:33 +0300

    feat: package class

---
+++ Translator/TranslatorPackage.cs
@@ -0,0 +1,25 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Translator.ToolWindows;
+
+namespace Translator
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideToolWindow(typeof(TranslatorWindow.Pane), Style = VsDockStyle.Tabbed, Window = WindowGuids.SolutionExplorer)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.TranslatorString)]
+    public sealed class TranslatorPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+            
+            this.RegisterToolWindows();
+        }
+    }
+}
